3d502d0343bf47fe1d6a61ee022f40579d516050
cantrk21 <cturk262@gmail.com>
2024-02-22 10:55:47 +0300

    T

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:zikirmatik/screens/home_screen.dart';
 
-void main(){
+void main() {
   runApp(const Uygulamam());
 }
 
-class Uygulamam extends StatelessWidget {   // bu widgeti calıstırmamız gerekıyor
+class Uygulamam extends StatelessWidget {
+  // bu Widgeti calıstırmamız gerekıyor
   const Uygulamam({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(  
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(fontFamily: "myfont"),
-          home : const HomeScreen(),
-     );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "myfont"),
+      home: const HomeScreen(),
+    );
   }
-
- 
-
-
 }
-
-
-
-
